9e4a9b7bac4c79ebdc689a4242936f105e0a3623
Amar Chatman <amarjerome@gmail.com>
2018-03-29 03:33:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,33 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+   word.match(/^[AEIOUaeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ending = []
+text_array = text.split(" ")
+text_array.each do |string|
+  if string.match(/(ing)\b/) || string.match(/\b(un)/)
+  ending << string
+else
+  false
+end
+end
+return ending
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
+#text.match(//\b[A-Z]) && text.match
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if !!text.match(/^[A-Z].*\W$/)
+  return true
+else
+  false
+end
 end
 
 def valid_phone_number?(phone)
